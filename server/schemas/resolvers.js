8a487af91bb1fff189d96e3c7a13e68f500cd357
Peter Tran <pettran866@gmail.com>
2022-08-12 16:07:19 -0700

    added query and mutation functionality to resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,74 @@
+// import models
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password');
+        
+        return userData;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    }
+  },
+  
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+    
+      const correctPw = await user.isCorrectPassword(password);
+    
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    // adds a book to the user's saveBooks array
+    // input has all of the book info
+    saveBook: async (parent, { input }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $push: { saveBooks: input } },
+          { new: true, runValidators: true }
+        )
+      }
+
+      return updatedUser;
+    },
+
+    // removes a book from the user's saveBooks array
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { saveBooks: { bookId: bookId } } },
+          { new: true, runValidators: true }
+        );
+      }
+
+      return updatedUser;
+    }
+  }
+}
+
+module.exports = resolvers;
