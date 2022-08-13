234a91bd41ded97a6bd20612328a9df447291845
Peter Tran <pettran866@gmail.com>
2022-08-13 12:07:37 -0700

    added REMOVE_BOOK mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -42,3 +42,22 @@ export const SAVE_BOOK = gql`
     }
   }
 `;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      user {
+        _id
+        username
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`
